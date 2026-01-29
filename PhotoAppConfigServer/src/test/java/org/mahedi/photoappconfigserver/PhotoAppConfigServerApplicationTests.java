8e5a5fd265c66054e65d3f6c8ad63e8b01cace0c
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-01-29 14:41:07 +0800

    added spring cloud config server application

---
+++ PhotoAppConfigServer/src/test/java/org/mahedi/photoappconfigserver/PhotoAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mahedi.photoappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
