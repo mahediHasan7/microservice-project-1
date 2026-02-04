55d98a80ee562cc720f4d36af68278faddb0828f
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2026-02-04 10:27:48 +0800

    added PhotoAppAlbum application

---
+++ PhotoAppAlbums/src/main/java/org/mahedi/photoappalbums/PhotoAppAlbumsApplication.java
@@ -0,0 +1,13 @@
+package org.mahedi.photoappalbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppAlbumsApplication.class, args);
+    }
+
+}
---
+++ PhotoAppAlbums/src/test/java/org/mahedi/photoappalbums/PhotoAppAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mahedi.photoappalbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAlbumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
